0f3ce4541f851826fe3fd0f432866e65be0c65da
xkyii <xkyii@qq.com>
2023-01-18 11:11:15 +0800

    Add: maven-shade-plugin

---
+++ xkyss-mocky/src/main/java/com/xkyss/mocky/entry/ApacheCommons3Entry.java
@@ -0,0 +1,9 @@
+package com.xkyss.mocky.entry;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class ApacheCommons3Entry {
+    public void used() {
+        StringUtils.isEmpty("Hello");
+    }
+}
