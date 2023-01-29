2abf5c02e0e42e244e41c1257ab0baaca48e8ff7
xkyii <xkyii@qq.com>
2023-01-29 09:54:21 +0800

    Add: ProGuard to core

--- xkyss-experimental/src/test/java/com/xkyss/experimental/DemoTest.java
+++ xkyss-experimental/src/test/java/com/xkyss/experimental/DemoTest.java
@@ -1,4 +1,11 @@
 package com.xkyss.experimental;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class DemoTest {
+    @Test
+    public void test() {
+        Assertions.assertTrue(true);
+    }
 }
