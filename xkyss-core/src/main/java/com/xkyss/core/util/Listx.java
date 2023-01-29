2b583bfefb2bf91225044b4030bfb427c87fa95e
xkyii <xkyii@qq.com>
2023-01-29 19:49:00 +0800

    Add: guava 依赖

--- xkyss-core/src/main/java/com/xkyss/core/util/Listx.java
+++ xkyss-core/src/main/java/com/xkyss/core/util/Listx.java
@@ -6,6 +6,13 @@ import java.util.List;
 
 public class Listx {
 
+    /**
+     * 安全获取元素
+     * @param list 列表
+     * @param index 索引
+     * @return 获取到的元素,如果越界,则返回<code>null</code>
+     * @param <E> 泛型类型
+     */
     public static <E> E get(List<E> list, int index) {
         if (CollectionUtils.isEmpty(list)) {
             return null;
---
+++ xkyss-core/src/main/java/com/xkyss/core/util/Objectx.java
@@ -0,0 +1,9 @@
+package com.xkyss.core.util;
+
+import com.google.common.base.Objects;
+
+public class Objectx {
+    public boolean equal(Object o1, Object o2) {
+        return Objects.equal(o1, o2);
+    }
+}
