6edb0fc0e7dee788b7854aaacc7c23b66b35f57d
xkyii <xkyii@qq.com>
2023-02-03 17:40:04 +0800

    Add: mocky Primes

---
+++ xkyss-mocky/src/main/java/com/xkyss/mocky/unit/types/Primes.java
@@ -0,0 +1,20 @@
+package com.xkyss.mocky.unit.types;
+
+import com.xkyss.mocky.abstraction.MockUnit;
+import com.xkyss.mocky.unit.objects.Froms;
+
+import static com.xkyss.mocky.contant.Alphabets.SMALL_PRIMES;
+
+public class Primes implements MockUnit<Integer> {
+
+    private final Froms froms;
+
+    public Primes(Froms froms) {
+        this.froms = froms;
+    }
+
+    @Override
+    public Integer get() {
+        return froms.from(SMALL_PRIMES).get();
+    }
+}
