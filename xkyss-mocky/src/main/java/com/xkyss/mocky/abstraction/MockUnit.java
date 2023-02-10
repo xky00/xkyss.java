b12af1bab3351aadfc2ceb736fb001ea4084a3c4
xkyii <xkyii@qq.com>
2023-02-10 14:15:51 +0800

    Add: mocky Hashes

--- xkyss-mocky/src/main/java/com/xkyss/mocky/abstraction/MockUnit.java
+++ xkyss-mocky/src/main/java/com/xkyss/mocky/abstraction/MockUnit.java
@@ -1,6 +1,9 @@
 package com.xkyss.mocky.abstraction;
 
+import java.util.function.Function;
 import java.util.function.Supplier;
+
+import static org.apache.commons.lang3.Validate.notNull;
 
 /**
  * Everything is MockUnit
@@ -10,4 +13,8 @@ import java.util.function.Supplier;
  */
 public interface MockUnit<T> extends Supplier<T> {
 
+    default <R> MockUnit<R> map(Function<T, R> function) {
+        notNull(function, "function");
+        return () -> function.apply(get());
+    }
 }
---
+++ xkyss-mocky/src/main/java/com/xkyss/mocky/unit/text/Hashes.java
@@ -0,0 +1,64 @@
+package com.xkyss.mocky.unit.text;
+
+import com.xkyss.mocky.abstraction.MockUnit;
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.function.UnaryOperator;
+
+public class Hashes implements MockUnit<String> {
+
+    private static final Integer HASHED_STRING_SIZE = 128;
+
+    private final Strings strings;
+    private final Random random;
+
+    public Hashes(Random random, Strings strings) {
+        this.random = random;
+        this.strings = strings;
+    }
+
+    @Override
+    public String get() {
+        List<MockUnit<String>> functions = Arrays.asList(
+            this::md2,
+            this::md5,
+            this::sha1,
+            this::sha256,
+            this::sha384,
+            this::sha512
+        );
+        int idx = random.nextInt(functions.size());
+        return functions.get(idx).get();
+    }
+
+    private MockUnit<String> supplier(UnaryOperator<String> digester) {
+        return strings.size(HASHED_STRING_SIZE).map(digester);
+    }
+
+    public String md2() {
+        return supplier(DigestUtils::md2Hex).get();
+    }
+
+    public String md5() {
+        return supplier(DigestUtils::md5Hex).get();
+    }
+
+    public String sha1() {
+        return supplier(DigestUtils::sha1Hex).get();
+    }
+
+    public String sha256() {
+        return supplier(DigestUtils::sha256Hex).get();
+    }
+
+    public String sha384() {
+        return supplier(DigestUtils::sha384Hex).get();
+    }
+
+    public String sha512() {
+        return supplier(DigestUtils::sha512Hex).get();
+    }
+}
