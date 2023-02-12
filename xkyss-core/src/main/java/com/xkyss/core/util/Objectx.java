69fa8885f3d923518effeeba2e75652972c2ca87
xkyii <xkyii@qq.com>
2023-02-12 10:55:41 +0800

    Del: Objectx

--- xkyss-core/src/main/java/com/xkyss/core/util/Objectx.java
+++
@@ -1,9 +0,0 @@
-package com.xkyss.core.util;
-
-import com.google.common.base.Objects;
-
-public class Objectx {
-    public boolean equal(Object o1, Object o2) {
-        return Objects.equal(o1, o2);
-    }
-}
