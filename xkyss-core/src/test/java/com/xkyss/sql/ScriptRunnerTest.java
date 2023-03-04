4dc60ea4720bdef908648ee22313d191eb898744
xkyii <xkyii@qq.com>
2023-03-04 22:58:59 +0800

    Up: to 2023.3.1

---
+++ xkyss-core/src/test/java/com/xkyss/sql/ScriptRunnerTest.java
@@ -0,0 +1,12 @@
+package com.xkyss.sql;
+
+import org.junit.jupiter.api.Test;
+
+
+public class ScriptRunnerTest {
+
+    @Test
+    public void test_01() {
+        // 见 xkyss-quarkus-demo test
+    }
+}
