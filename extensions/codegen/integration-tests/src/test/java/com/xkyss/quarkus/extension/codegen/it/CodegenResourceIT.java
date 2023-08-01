6a641d52c4f3cb1719e89eb4d07d5c77b1c0365e
xkyii <xkyii@qq.com>
2023-08-01 16:45:21 +0800

    Del: 旧项目

--- extensions/codegen/integration-tests/src/test/java/com/xkyss/quarkus/extension/codegen/it/CodegenResourceIT.java
+++
@@ -1,7 +0,0 @@
-package com.xkyss.quarkus.extension.codegen.it;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class CodegenResourceIT extends CodegenResourceTest {
-}
