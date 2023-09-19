2842fcb63761926b48edf80a96d8dfb98ff493e6
xkyss <xkyss@foxmail.com>
2023-09-19 17:22:02 +0800

    Up: UniCommand

--- playground/quarkus-cli/src/main/java/com/xkyss/exp/EntryCommand.java
+++ playground/quarkus-cli/src/main/java/com/xkyss/exp/EntryCommand.java
@@ -7,13 +7,19 @@
 package com.xkyss.exp;
 
 import io.quarkus.picocli.runtime.annotations.TopCommand;
+import io.smallrye.mutiny.Uni;
 import jakarta.inject.Inject;
 import org.jboss.logging.Logger;
 import picocli.CommandLine;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 
 @TopCommand
-@CommandLine.Command(mixinStandardHelpOptions = true, subcommands = {HelloCommand.class, GoodByeCommand.class})
+@CommandLine.Command(mixinStandardHelpOptions = true, subcommands = {HelloCommand.class, GoodByeCommand.class, UniCommand.class})
 public class EntryCommand {
 }
 
@@ -37,5 +43,44 @@ class GoodByeCommand implements Runnable {
     @Override
     public void run() {
         logger.info("Goodbye World!!");
+    }
+}
+
+@CommandLine.Command(name = "uni", description = "Something about Mitiny")
+class UniCommand implements Runnable {
+    @Inject
+    Logger logger;
+
+    @Override
+    public void run() {
+        logger.info("Hello Uni");
+        List<Long> roles = List.of(1L, 2L);
+
+        Set<String> perms = new HashSet<>();
+        Uni.combine().all()
+            .unis(roles.stream()
+                .map(roleId -> selectMenuPermsByRoleId(roleId)
+                    .onItem().invoke(pers -> {
+                        logger.info(String.join(",", pers));
+                    }))
+                .collect(Collectors.toList()))
+            .combinedWith(permsLists -> {
+                @SuppressWarnings("unchecked")
+                List<Set<String>> lists = (List<Set<String>>) permsLists;
+                lists.forEach(perms::addAll);
+                return perms;
+            })
+            .subscribe().with(ps -> {
+                logger.info("All perms: ");
+                logger.info(String.join(",", ps));
+            });
+    }
+
+    private Uni<Set<String>> selectMenuPermsByRoleId(Long id) {
+        return Uni.createFrom().item(Set.of(
+            "perm-0",
+            String.format("perm-%d", id),
+            String.format("perm-%d%d", id, id)
+        ));
     }
 }
