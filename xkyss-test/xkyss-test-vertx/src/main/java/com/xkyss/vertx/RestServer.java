530e2614de7053d597fff4580189435e2ad1a8b8
xkyii <xkyii@foxmail.com>
2024-04-02 21:21:35 +0800

    Up: vertx rest 同步请求测试

---
+++ xkyss-test/xkyss-test-vertx/src/main/java/com/xkyss/vertx/RestServer.java
@@ -0,0 +1,100 @@
+package com.xkyss.vertx;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RestServer extends AbstractVerticle {
+
+    public static void main(String[] args) {
+        VertxOptions options = new VertxOptions();
+        options.setMaxEventLoopExecuteTimeUnit(java.util.concurrent.TimeUnit.SECONDS);
+        options.setWarningExceptionTime(2000);
+        options.setMaxWorkerExecuteTimeUnit(java.util.concurrent.TimeUnit.SECONDS);
+        options.setMaxWorkerExecuteTime(2000);
+        Vertx vertx = Vertx.vertx(options);
+        vertx.deployVerticle(new RestServer());
+    }
+    private Map<String, JsonObject> products = new HashMap<>();
+
+    @Override
+    public void start() {
+
+        setUpInitialData();
+
+        Router router = Router.router(vertx);
+
+        router.route().handler(BodyHandler.create());
+        router.get("/products/:productID").handler(this::handleGetProduct);
+        router.put("/products/:productID").handler(this::handleAddProduct);
+        router.get("/products").handler(this::handleListProducts);
+
+        vertx.createHttpServer().requestHandler(router).listen(8080);
+        System.out.println("Start server on port 8080");
+    }
+
+    private void handleGetProduct(RoutingContext routingContext) {
+        String productID = routingContext.request().getParam("productID");
+        HttpServerResponse response = routingContext.response();
+        if (productID == null) {
+            sendError(400, response);
+        } else {
+            JsonObject product = products.get(productID);
+            if (product == null) {
+                sendError(404, response);
+            } else {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                response.putHeader("content-type", "application/json").end(product.encodePrettily());
+            }
+        }
+    }
+
+    private void handleAddProduct(RoutingContext routingContext) {
+        String productID = routingContext.request().getParam("productID");
+        HttpServerResponse response = routingContext.response();
+        if (productID == null) {
+            sendError(400, response);
+        } else {
+            JsonObject product = routingContext.body().asJsonObject();
+            if (product == null) {
+                sendError(400, response);
+            } else {
+                products.put(productID, product);
+                response.end();
+            }
+        }
+    }
+
+    private void handleListProducts(RoutingContext routingContext) {
+        JsonArray arr = new JsonArray();
+        products.forEach((k, v) -> arr.add(v));
+        routingContext.response().putHeader("content-type", "application/json").end(arr.encodePrettily());
+    }
+
+    private void sendError(int statusCode, HttpServerResponse response) {
+        response.setStatusCode(statusCode).end();
+    }
+
+    private void setUpInitialData() {
+        addProduct(new JsonObject().put("id", "prod3568").put("name", "Egg Whisk").put("price", 3.99).put("weight", 150));
+        addProduct(new JsonObject().put("id", "prod7340").put("name", "Tea Cosy").put("price", 5.99).put("weight", 100));
+        addProduct(new JsonObject().put("id", "prod8643").put("name", "Spatula").put("price", 1.00).put("weight", 80));
+    }
+
+    private void addProduct(JsonObject product) {
+        products.put(product.getString("id"), product);
+    }
+}
