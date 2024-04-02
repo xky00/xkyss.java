7e300ec79c806b502589d68f28e4ee687e8d16b2
xkyii@th <xkyii@foxmail.com>
2024-04-02 12:04:23 +0800

    Add: test: jackson 转换对象为Map

--- xkyss-test/xkyss-test-vertx/src/test/java/com/xkyss/vertx/ObjectMapperTest.java
+++ xkyss-test/xkyss-test-vertx/src/test/java/com/xkyss/vertx/ObjectMapperTest.java
@@ -1,11 +1,14 @@
 package com.xkyss.vertx;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import io.vertx.core.json.jackson.DatabindCodec;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 public class ObjectMapperTest {
     @Test
@@ -15,6 +18,20 @@ public class ObjectMapperTest {
 
         String s = om.writeValueAsString(new Foo("nameValue", "codeValue"));
         Assertions.assertEquals("{\"name.key\":\"nameValue\",\"code.key\":\"codeValue\"}", s);
+    }
+
+    @Test
+    public void test_02() {
+        ObjectMapper om = DatabindCodec.mapper();
+        om.setPropertyNamingStrategy(PropertyNamingStrategies.LOWER_DOT_CASE);
+
+        Map<String, String> map = om
+            .convertValue(new Foo("nameValue", "codeValue"), new TypeReference<Map<String, String>>() {});
+
+        Assertions.assertNotNull(map);
+        Assertions.assertEquals(2, map.size());
+        Assertions.assertTrue(map.containsKey("name.key"));
+        Assertions.assertTrue(map.containsKey("code.key"));
     }
 
 
