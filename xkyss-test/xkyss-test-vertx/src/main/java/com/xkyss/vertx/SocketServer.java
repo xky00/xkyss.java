5595865661a1194a909c2fc4544a56e483105d39
xkyii@th <xkyii@foxmail.com>
2024-05-08 18:31:17 +0800

    Up: 粘包处理

--- xkyss-test/xkyss-test-vertx/src/main/java/com/xkyss/vertx/SocketServer.java
+++ xkyss-test/xkyss-test-vertx/src/main/java/com/xkyss/vertx/SocketServer.java
@@ -6,6 +6,9 @@ import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.logging.SLF4JLogDelegateFactory;
 import io.vertx.core.net.NetServer;
 import io.vertx.core.net.NetServerOptions;
+import io.vertx.core.parsetools.JsonEventType;
+import io.vertx.core.parsetools.JsonParser;
+import io.vertx.core.parsetools.RecordParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,9 +36,32 @@ public class SocketServer extends AbstractVerticle {
 
         server
             .connectHandler(socket -> {
-                socket.handler(buffer -> {
-                    logger.info("Received data: {}", buffer.toString("UTF-8"));
-                });
+                // 换行粘包处理
+                // RecordParser.newDelimited("\n", socket)
+                //     .endHandler(v -> socket.close())
+                //     .exceptionHandler(e -> {
+                //         logger.error("Error", e);
+                //         socket.close();
+                //     })
+                //     .handler(buffer -> {
+                //         String data = buffer.toString("UTF-8");
+                //         logger.info("Received data: {}", data);
+                //         // socket.write("Hello " + name + "\n", "UTF-8");
+                //     });
+
+                // Json粘包处理
+                JsonParser parser = JsonParser.newParser(socket);
+                parser.objectValueMode()
+                    .endHandler(v -> socket.close())
+                    .exceptionHandler(e -> {
+                        logger.error("Error", e);
+                        socket.close();
+                    })
+                    .handler(event -> {
+                        Message message = event.mapTo(Message.class);
+                        logger.info("Received message: {}", message.toString());
+
+                    });
             })
             .listen();
 
@@ -51,5 +77,34 @@ public class SocketServer extends AbstractVerticle {
                     logger.error("Failed to subscribe to mlcache-demo", res.cause());
                 }
             });
+    }
+
+    static class Message {
+        private String data;
+        private String index;
+
+        public String getData() {
+            return data;
+        }
+
+        public void setData(String data) {
+            this.data = data;
+        }
+
+        public String getIndex() {
+            return index;
+        }
+
+        public void setIndex(String index) {
+            this.index = index;
+        }
+
+        @Override
+        public String toString() {
+            return "Message{" +
+                "data='" + data + '\'' +
+                ", index='" + index + '\'' +
+                '}';
+        }
     }
 }
