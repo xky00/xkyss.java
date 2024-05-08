366f64a9cbe78868c5d678598df21ecf3e7c84e9
xkyii@th <xkyii@foxmail.com>
2024-05-08 14:00:41 +0800

    Up: 增加logging支持

--- xkyss-test/xkyss-test-vertx/src/main/java/com/xkyss/vertx/SocketClient.java
+++ xkyss-test/xkyss-test-vertx/src/main/java/com/xkyss/vertx/SocketClient.java
@@ -2,19 +2,31 @@ package com.xkyss.vertx;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.logging.SLF4JLogDelegateFactory;
 import io.vertx.core.net.NetClient;
 import io.vertx.core.net.NetClientOptions;
-import io.vertx.core.net.NetSocket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static io.vertx.core.logging.LoggerFactory.LOGGER_DELEGATE_FACTORY_CLASS_NAME;
+import static java.lang.System.setProperty;
 
 public class SocketClient extends AbstractVerticle {
 
     public static void main(String[] args) {
+        setProperty (LOGGER_DELEGATE_FACTORY_CLASS_NAME, SLF4JLogDelegateFactory.class.getName());
+
         Vertx vertx = Vertx.vertx(VertxKit.debugOptions());
         vertx.deployVerticle(new SocketClient());
     }
 
+    private final Logger logger = LoggerFactory.getLogger(SocketClient.class);
+
     @Override
     public void start() {
+        logger.info("starting...");
+
         NetClientOptions options = new NetClientOptions()
             .setLogActivity(true)
             .setConnectTimeout(10000);
@@ -24,11 +36,14 @@ public class SocketClient extends AbstractVerticle {
             .connect(4321, "localhost")
             .onSuccess(socket -> {
                 socket.handler(buffer -> {
-                    System.out.println("Received data: " + buffer.toString("UTF-8"));
+                    logger.info("Received data: {}", buffer.toString("UTF-8"));
                 });
                 socket.write("Hello");
             })
-            .onFailure(e -> System.out.println("Failed to connect: " + e.getMessage()))
+            .onFailure(e -> logger.error("Failed to connect.", e));
             ;
+
+        EventBus eb = vertx.eventBus();
+        eb.send("mlcache-demo", "yay! Someone kicked a ball.");
     }
 }
--- xkyss-test/xkyss-test-vertx/src/main/java/com/xkyss/vertx/SocketServer.java
+++ xkyss-test/xkyss-test-vertx/src/main/java/com/xkyss/vertx/SocketServer.java
@@ -2,15 +2,26 @@ package com.xkyss.vertx;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.logging.SLF4JLogDelegateFactory;
 import io.vertx.core.net.NetServer;
 import io.vertx.core.net.NetServerOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static io.vertx.core.logging.LoggerFactory.LOGGER_DELEGATE_FACTORY_CLASS_NAME;
+import static java.lang.System.setProperty;
 
 public class SocketServer extends AbstractVerticle {
 
     public static void main(String[] args) {
+        setProperty (LOGGER_DELEGATE_FACTORY_CLASS_NAME, SLF4JLogDelegateFactory.class.getName());
+
         Vertx vertx = Vertx.vertx(VertxKit.debugOptions());
         vertx.deployVerticle(new SocketServer());
     }
+
+    private final Logger logger = LoggerFactory.getLogger(SocketClient.class);
 
     @Override
     public void start() {
@@ -23,9 +34,22 @@ public class SocketServer extends AbstractVerticle {
         server
             .connectHandler(socket -> {
                 socket.handler(buffer -> {
-                    System.out.println("Received data: " + buffer.toString("UTF-8"));
+                    logger.info("Received data: {}", buffer.toString("UTF-8"));
                 });
             })
             .listen();
+
+        EventBus eb = vertx.eventBus();
+        eb.consumer("mlcache-demo")
+            .handler(message -> {
+                logger.info("Received message: {}", message.body());
+            })
+            .completionHandler(res -> {
+                if (res.succeeded()) {
+                    logger.info("Subscribed to mlcache-demo");
+                } else {
+                    logger.error("Failed to subscribe to mlcache-demo", res.cause());
+                }
+            });
     }
 }
