b28159d1ab9c00f9f250df0a55a5627f4a8067b7
xkyii@th <xkyii@foxmail.com>
2024-09-13 17:53:20 +0800

    Add: redis test

--- xkyss-test/xkyss-test-redis/src/test/java/com/xkyss/redis/RedisTest.java
+++ xkyss-test/xkyss-test-redis/src/test/java/com/xkyss/redis/RedisTest.java
@@ -374,4 +374,30 @@ public class RedisTest {
             Assertions.assertTrue(testContext.awaitCompletion(1, TimeUnit.SECONDS));
         }
     }
+
+    @Test
+    public void test_acl() throws InterruptedException {
+        Redis client = Redis.createClient(vertx, REDIS_HOST);
+        RedisAPI redis = RedisAPI.api(client);
+
+        {
+            VertxTestContext testContext = new VertxTestContext();
+            client.connect()
+                .onComplete(testContext.succeedingThenComplete())
+                .onSuccess(r -> System.out.println("connect ok: " + r))
+                .onFailure(e -> System.out.println("connect fail: " + e))
+            ;
+            Assertions.assertTrue(testContext.awaitCompletion(1, TimeUnit.SECONDS));
+        }
+
+        {
+            VertxTestContext testContext = new VertxTestContext();
+            redis.acl(Collections.singletonList("CAT"))
+                .onComplete(testContext.succeedingThenComplete())
+                .onSuccess(r -> System.out.println("[acl CAT] ok: " + r))
+                .onFailure(e -> System.out.println("connect fail: " + e))
+            ;
+            Assertions.assertTrue(testContext.awaitCompletion(1, TimeUnit.SECONDS));
+        }
+    }
 }
