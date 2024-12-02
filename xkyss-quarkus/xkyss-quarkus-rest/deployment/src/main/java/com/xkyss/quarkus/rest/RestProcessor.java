381e672e25f512287f85ff8a38b0ef9dcb47b4f7
xkyii@th <xkyii@foxmail.com>
2024-12-02 13:10:27 +0800

    Up:  xkyss-quarkus-server 调整为 xkyss-quarkus-rest

---
+++ xkyss-quarkus/xkyss-quarkus-rest/deployment/src/main/java/com/xkyss/quarkus/rest/RestProcessor.java
@@ -0,0 +1,33 @@
+package com.xkyss.quarkus.rest;
+
+import io.quarkus.arc.deployment.SyntheticBeanBuildItem;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.annotations.ExecutionTime;
+import io.quarkus.deployment.annotations.Record;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
+import jakarta.inject.Singleton;
+import jakarta.validation.MessageInterpolator;
+import org.jboss.logging.Logger;
+
+public class RestProcessor {
+
+    private static final Logger LOGGER = Logger.getLogger(RestProcessor.class.getName());
+
+    private static final String FEATURE = "xkyss-server";
+
+    @BuildStep
+    FeatureBuildItem feature() {
+        LOGGER.info(FEATURE);
+
+        return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    @Record(ExecutionTime.STATIC_INIT)
+    SyntheticBeanBuildItem registerMessageInterpolator(RestRecorder ksServerRecorder) {
+        return SyntheticBeanBuildItem.configure(MessageInterpolator.class)
+            .scope(Singleton.class)
+            .supplier(ksServerRecorder.messageInterpolatorSupplier())
+            .done();
+    }
+}
---
+++ xkyss-quarkus/xkyss-quarkus-rest/runtime/src/main/java/com/xkyss/quarkus/rest/config/RuntimeConfig.java
@@ -0,0 +1,89 @@
+package com.xkyss.quarkus.rest.config;
+
+import io.quarkus.runtime.annotations.ConfigPhase;
+import io.quarkus.runtime.annotations.ConfigRoot;
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithConverter;
+import io.smallrye.config.WithDefault;
+import io.vertx.core.http.HttpMethod;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static com.xkyss.quarkus.rest.constant.Constants.CONFIG_REST_PREFIX;
+
+@ConfigMapping(prefix = CONFIG_REST_PREFIX)
+@ConfigRoot(phase = ConfigPhase.RUN_TIME)
+public interface RuntimeConfig {
+
+    /**
+     * Response filter config
+     * @return config
+     */
+    Map<String, ResponseFilterConfig> responseFilter();
+
+    /**
+     * Http log filter config
+     * @return config
+     */
+    Map<String, HttpLogFilterConfig> httpLogFilter();
+
+    /**
+     * Response filter config
+     */
+    interface ResponseFilterConfig {
+
+        /**
+         * Enable or disable
+         * @return enable or disable
+         */
+        @WithDefault("true")
+        boolean enabled();
+
+        /**
+         * Path pattern
+         * @return path pattern
+         */
+        @WithDefault("/*")
+        @WithConverter(LowerConverter.class)
+        Optional<String> path();
+
+        /**
+         * Http methods
+         * @return http methods
+         */
+        @WithDefault("*")
+        @WithConverter(UpperListConverter.class)
+        Optional<List<String>> methods();
+    }
+
+    /**
+     * Http log filter config
+     */
+    interface HttpLogFilterConfig {
+
+        /**
+         * Enable or disable
+         * @return enable or disable
+         */
+        @WithDefault("true")
+        boolean enabled();
+
+        /**
+         * Path pattern
+         * @return path pattern
+         */
+        @WithDefault("/*")
+        @WithConverter(LowerConverter.class)
+        Optional<String> path();
+
+        /**
+         * Http methods
+         * @return http methods
+         */
+        @WithDefault("*")
+        @WithConverter(UpperListConverter.class)
+        Optional<List<String>> methods();
+    }
+}
